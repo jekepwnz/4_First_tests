745e78990bd098b9a0478c36e454910081e9a0cf
Evgeniy Volkov <hardcoreokje@gmail.com>
2025-02-02 16:51:53 +0300

    added BeforeAll and BeforeEach methods

--- src/test/java/JUnitExamples.java
+++ src/test/java/JUnitExamples.java
@@ -1,9 +1,26 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class JUnitExamples {
 
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Its done before all tests");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("    Some action before each test");
+    }
+
     @Test
     void firstTest() {
-        System.out.println("There is the first test");
+        System.out.println("        There is the first test");
     }
+    @Test
+    void secondTest() {
+        System.out.println("        There is second test");
+    }
+
 }
