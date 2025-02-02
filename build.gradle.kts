d41be90db0a424b4d7281be261ec28c5acaa8e7e
Evgeniy Volkov <hardcoreokje@gmail.com>
2025-02-02 21:50:45 +0300

    added test configuration and completed fillFromTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("com.codeborne:selenide:7.7.0")
+    testImplementation("io.github.bonigarcia:webdrivermanager:5.5.3")
+    testImplementation("org.slf4j:slf4j-simple:2.0.16")
 }
 
 tasks.test {
--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,9 +1,39 @@
+import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTests {
-    // https://demoqa.com/text-box
-//    @Test
-//    void fillFormTest() {
-//
+
+    public static String pageLoadStrategy = "none";
+    @BeforeAll
+    static void configureBrowser() {
+
+//        WebDriverManager.chromedriver().driverVersion("132.0.6834.159").setup();
+//        Configuration.browser = "chrome";
+//        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+//        Configuration.timeout = 20000;
+//        Configuration.pageLoadTimeout = 20000;
+    }
+
+    @Test
+    void fillFormTest() {
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("Jack Sparrow");
+        $("#userEmail").setValue("JackSparrow@gmail.com");
+        $("#currentAddress").setValue("I will not provide my address");
+        $("#permanentAddress").setValue("Im sure i will not tell you where i am living");
+        $("#submit").scrollIntoView(true).click();
+
+        $("#output").shouldHave(text("Jack Sparrow"),text("JackSparrow@gmail.com"),
+                text("I will not provide my address"),text("Im sure i will not tell you where i am living"));
+
+
     }
 }
