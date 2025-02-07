4dcb3108b9032148f866c4d4aca657d5114c2d85
Evgeniy Volkov <hardcoreokje@gmail.com>
2025-02-08 00:00:15 +0300

    SelenideWiki junit5 check added

---
+++ src/test/java/SelenideWiki.java
@@ -0,0 +1,20 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideWiki {
+
+    @Test
+    void wikiCheck() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".markdown-body ul").$(byText("Soft assertions")).shouldBe(Condition.exist);
+        $(".markdown-body ul").$(byText("Soft assertions")).click();
+        $(".markdown-body").shouldHave(text("junit5"));
+
+    }
+}
