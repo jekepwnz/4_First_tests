75c9d1db6975e101bf85209c90913a4a9c25c8ae
Evgeniy Volkov <hardcoreokje@gmail.com>
2025-02-08 15:49:53 +0300

    added DragAndDrop test

---
+++ src/test/java/DragAndDrop.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDrop {
+    @Test
+    void DragAndDropWithActions() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+
+        $("#column-a").$("header").shouldHave(text("B"));
+        $("#column-b").$("header").shouldHave(text("A"));
+
+    }
+
+    @Test
+    void DragAndDropWithMethod() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDrop(to("#column-b"));
+
+        $("#column-a").$("header").shouldHave(text("B"));
+        $("#column-b").$("header").shouldHave(text("A"));
+        $("#column-a").dragAndDrop(to("#column-b"));
+
+    }
+}
